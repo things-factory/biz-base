2f82596bf19a76586a963d792fee2510418e9af0
leemyunggyu <jaylee@hatiolab.com>
2019-05-30 21:05:54 +0900

    v0.0.20

--- server/graphql/resolvers/company/companies.ts
+++ server/graphql/resolvers/company/companies.ts
@@ -1,48 +1,13 @@
-import { conditionBuilder, ListParams } from '@things-factory/shell'
+import { listQueryBuilder, ListParams } from '@things-factory/shell'
 import { getRepository } from 'typeorm'
 import { Company } from '../../../entities'
 
 export const companiesResolver = {
   async companies(_: any, params: ListParams, context: any, args: any) {
     const queryBuilder = getRepository(Company).createQueryBuilder()
-
-    const filters = params.filters
-    const pagination = params.pagination
-    const sortings = params.sortings
-
-    if (filters && filters.length > 0) {
-      filters.forEach((filter, index: number) => {
-        const condition = conditionBuilder(filter.name, filter.operator, filter.value)
-        if (index === 0) {
-          queryBuilder.where(condition.clause)
-          if (condition.parameters) queryBuilder.setParameters(condition.parameters)
-        } else {
-          queryBuilder.andWhere(condition.clause)
-          if (condition.parameters) queryBuilder.setParameters(condition.parameters)
-        }
-      })
-    }
-
-    if (pagination && pagination.skip >= 0 && pagination.take >= 0) {
-      queryBuilder.skip(pagination.skip)
-      queryBuilder.take(pagination.take)
-    }
-
-    if (sortings && sortings.length > 0) {
-      sortings.forEach((sorting, index) => {
-        if (index === 0) {
-          queryBuilder.orderBy(sorting.name, sorting.desc ? 'DESC' : 'ASC')
-        } else {
-          queryBuilder.addOrderBy(sorting.name, sorting.desc ? 'DESC' : 'ASC')
-        }
-      })
-    }
-
+    listQueryBuilder(queryBuilder, params)
     const [items, total] = await queryBuilder.getManyAndCount()
 
-    return {
-      items,
-      total
-    }
+    return { items, total }
   }
 }
