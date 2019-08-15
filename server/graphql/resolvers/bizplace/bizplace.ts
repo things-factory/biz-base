5fef1997a2ff903b878d5f05c8451e2685862f7b
leemyunggyu <jaylee@hatiolab.com>
2019-08-15 20:35:34 +0800

    v0.1.9

--- server/graphql/resolvers/bizplace/bizplace.ts
+++ server/graphql/resolvers/bizplace/bizplace.ts
@@ -5,7 +5,7 @@ export const bizplaceResolver = {
   async bizplace(_: any, { name }) {
     return await getRepository(Bizplace).findOne({
       where: { name },
-      relations: ['creator', 'updater']
+      relations: ['company', 'creator', 'updater']
     })
   }
 }
--- server/graphql/resolvers/bizplace/bizplaces.ts
+++ server/graphql/resolvers/bizplace/bizplaces.ts
@@ -7,6 +7,7 @@ export const bizplacesResolver = {
     const queryBuilder = getRepository(Bizplace).createQueryBuilder()
     buildQuery(queryBuilder, params, context)
     const [items, total] = await queryBuilder
+      .leftJoinAndSelect('Bizplace.company', 'Company')
       .leftJoinAndSelect('Bizplace.creator', 'Creator')
       .leftJoinAndSelect('Bizplace.updater', 'Updater')
       .getManyAndCount()
