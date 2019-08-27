06b6effcc4ee4fbc3ba7c31acedb81a6cdcf3ef1
Hearty Oh <heartyoh@hatiolab.com>
2019-08-27 21:16:05 +0900

    v0.1.36

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,14 @@
 export * from './entities'
 export * from './graphql'
+
+process.on('bootstrap-module-register-context' as any, function(app, contextList) {
+  contextList.push(async function({ ctx }) {
+    ctx.bizplace = {
+      id: 'kimeda',
+      name: 'Kimeda',
+      __type: 'bizplace'
+    }
+
+    return ctx
+  })
+})
