f417665bdc308c1ba5bce13871c999ca66b28d3b
leemyunggyu <jaylee@hatiolab.com>
2019-08-27 18:06:24 +0900

    v0.1.34

--- server/graphql/resolvers/bizplace/bizplaces.ts
+++ server/graphql/resolvers/bizplace/bizplaces.ts
@@ -5,7 +5,7 @@ import { Bizplace } from '../../../entities'
 export const bizplacesResolver = {
   async bizplaces(_: any, params: ListParam, context: any) {
     const queryBuilder = getRepository(Bizplace).createQueryBuilder()
-    buildQuery(queryBuilder, params, context)
+    buildQuery(queryBuilder, params, context, context && context.domain && !context.domain.systemFlag)
     const [items, total] = await queryBuilder
       .leftJoinAndSelect('Bizplace.company', 'Company')
       .leftJoinAndSelect('Bizplace.creator', 'Creator')
