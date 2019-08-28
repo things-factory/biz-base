2eda9538c6588801f625892d2a7591944a6baf5d
leemyunggyu <jaylee@hatiolab.com>
2019-08-28 21:36:25 +0900

    v0.1.47

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,64 @@
+import { getRepository } from 'typeorm'
+import { Bizplace } from './entities'
+
 export * from './entities'
 export * from './graphql'
 export * from './migrations'
 
 import './middlewares'
+
+export async function getUserBizplaces(context: any) {
+  if (context && context.state && context.state.user && context.state.user.id) {
+    const userId = context.state.user.id
+    let bizplaces = []
+    try {
+      // For sqlite
+      bizplaces = await getRepository(Bizplace).query(
+        `
+          SELECT
+            id,
+            name,
+            description
+          FROM
+            bizplaces
+          WHERE
+            id
+          IN (
+            SELECT
+              bizplaces_id
+            FROM
+              bizplaces_users
+            WHERE
+              users_id = :userId
+          )
+        `,
+        [userId]
+      )
+    } catch (e) {
+      // For postgres temporarly
+      bizplaces = await getRepository(Bizplace).query(
+        `
+          SELECT
+            id,
+            name,
+            description
+          FROM
+            bizplaces
+          WHERE
+            id
+          IN (
+            SELECT
+              bizplaces_id
+            FROM
+              bizplaces_users
+            WHERE
+              users_id = $1
+          )
+        `,
+        [userId]
+      )
+    } finally {
+      return bizplaces
+    }
+  }
+}
