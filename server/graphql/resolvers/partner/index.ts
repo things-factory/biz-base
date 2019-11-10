11b89247b5b08b8e1706e1c367c98c5f87956b80
Hearty Oh <heartyoh@hatiolab.com>
2019-11-11 00:50:32 +0900

    gql import

--- server/graphql/resolvers/partner/index.ts
+++ server/graphql/resolvers/partner/index.ts
@@ -5,7 +5,7 @@ import { partnerCustomer } from './partner-customer'
 import { partnerVendors } from './partner-vendors'
 import { partnerVendor } from './partner-vendor'
 
-export const Query = { partnerCustomers, partnerCustomer, partnerVendors, partnerVendor }
+export const Query = { ...partnerCustomers, ...partnerCustomer, ...partnerVendors, ...partnerVendor }
 
 export const Mutation = {
   ...createPartner,
