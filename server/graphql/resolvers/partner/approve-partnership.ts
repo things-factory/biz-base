50831e12b832d6edd1e75493563fe13fdbeacf4f
leemyunggyu <jaylee@hatiolab.com>
2019-11-20 17:31:58 +0900

    v1.0.0-alpha.11

--- server/graphql/resolvers/partner/approve-partnership.ts
+++ server/graphql/resolvers/partner/approve-partnership.ts
@@ -1,19 +1,20 @@
 import { User } from '@things-factory/auth-base'
-import { EntityManager, getManager, getRepository } from 'typeorm'
+import { EntityManager, getManager, getRepository, Repository } from 'typeorm'
 import { PARTNERSHIP_STATUS } from '../../../constants/partnership-status'
 import { Partner } from '../../../entities'
 
 export const approvePartnership = {
   async approvePartnership(_: any, { id }, context: any): Promise<void> {
     getManager().transaction(async (trxMgr: EntityManager) => {
-      await approve(id, context.state.user)
+      await approve(id, context.state.user, trxMgr)
     })
   }
 }
 
-export const approve = async function(id: string, user: User): Promise<void> {
-  const partner: Partner = await getRepository(Partner).findOne(id)
-  await getRepository(Partner).save({
+export const approve = async function(id: string, user: User, trxMgr?: EntityManager): Promise<void> {
+  const partnerRepo: Repository<Partner> = trxMgr ? trxMgr.getRepository(Partner) : getRepository(Partner)
+  const partner: Partner = await partnerRepo.findOne(id)
+  await partnerRepo.save({
     ...partner,
     activated: true,
     status: PARTNERSHIP_STATUS.APPROVED,
--- server/graphql/resolvers/partner/reject-partnership.ts
+++ server/graphql/resolvers/partner/reject-partnership.ts
@@ -1,19 +1,20 @@
 import { User } from '@things-factory/auth-base'
-import { EntityManager, getManager, getRepository } from 'typeorm'
+import { EntityManager, getManager, Repository, getRepository } from 'typeorm'
 import { PARTNERSHIP_STATUS } from '../../../constants/partnership-status'
 import { Partner } from '../../../entities'
 
 export const rejectPartnership = {
   async rejectPartnership(_: any, { id }, context: any): Promise<void> {
     await getManager().transaction(async (trxMgr: EntityManager) => {
-      await reject(id, context.state.user)
+      await reject(id, context.state.user, trxMgr)
     })
   }
 }
 
-export const reject = async function(id: string, user: User): Promise<void> {
-  const partner: Partner = await getRepository(Partner).findOne(id)
-  await getRepository(Partner).save({
+export const reject = async function(id: string, user: User, trxMgr?: EntityManager): Promise<void> {
+  const partnerRepo: Repository<Partner> = trxMgr ? trxMgr.getRepository(Partner) : getRepository(Partner)
+  const partner: Partner = await partnerRepo.findOne(id)
+  await partnerRepo.save({
     ...partner,
     status: PARTNERSHIP_STATUS.REJECTED,
     updater: user
