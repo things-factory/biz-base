8d7ce7496e94b0152dce3d55aa3384f14bc93041
leemyunggyu <jaylee@hatiolab.com>
2019-11-26 10:39:45 +0900

    v1.0.0-alpha.20

--- server/graphql/resolvers/company/companies.ts
+++ server/graphql/resolvers/company/companies.ts
@@ -1,16 +1,14 @@
-import { buildQuery, ListParam } from '@things-factory/shell'
-import { getRepository } from 'typeorm'
+import { convertListParams, ListParam } from '@things-factory/shell'
+import { FindManyOptions, getRepository } from 'typeorm'
 import { Company } from '../../../entities'
 
 export const companiesResolver = {
-  async companies(_: any, params: ListParam) {
-    const queryBuilder = getRepository(Company).createQueryBuilder()
-    buildQuery(queryBuilder, params)
-    const [items, total] = await queryBuilder
-      .leftJoinAndSelect('Company.creator', 'Creator')
-      .leftJoinAndSelect('Company.updater', 'Updater')
-      .getManyAndCount()
-
+  async companies(_: any, params: ListParam, _context: any) {
+    const convertedParams: FindManyOptions<Company> = convertListParams(params)
+    const [items, total] = await getRepository(Company).findAndCount({
+      ...convertedParams,
+      relations: ['creator', 'updater']
+    })
     return { items, total }
   }
 }
