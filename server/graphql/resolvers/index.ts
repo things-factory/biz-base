08d7f0aa0ee522a08eed61a97fda922fc52f8064
leemyunggyu <jaylee@hatiolab.com>
2019-12-19 17:29:07 +0900

    v1.0.0-alpha.40

--- server/graphql/resolvers/index.ts
+++ server/graphql/resolvers/index.ts
@@ -1,6 +1,7 @@
 import * as BizOption from './biz-option'
 import * as BizOptionDetail from './biz-option-detail'
 import * as Bizplace from './bizplace'
+import * as BizplaceRole from './bizplace-role'
 import * as Company from './company'
 import * as ContactPoint from './contact-point'
 import * as Manager from './manager'
@@ -9,6 +10,7 @@ import * as Worker from './worker'
 
 export const queries = [
   Bizplace.Query,
+  BizplaceRole.Query,
   Company.Query,
   ContactPoint.Query,
   Worker.Query,
