6457cdb460665ad19c0e523ebce8bb1e62ad3acd
leemyunggyu <jaylee@hatiolab.com>
2019-12-19 19:27:57 +0900

    v1.0.0-alpha.41

--- server/graphql/resolvers/bizplace-role/assigned-roles-by-user.ts
+++ server/graphql/resolvers/bizplace-role/assigned-roles-by-user.ts
@@ -1,11 +1,14 @@
 import { Role, User } from '@things-factory/auth-base'
 import { Domain } from '@things-factory/shell'
 import { EntityManager, getRepository, Repository } from 'typeorm'
-import { Bizplace, BizplaceUser } from '../../../entities'
 import { rolesByUserBizplace } from './roles-by-user-bizplace'
 
 export const assignedRolesByUserResolver = {
   async assignedRolesByUser(_: any, { user }, context: any) {
+    if (user?.id) {
+      user = await getRepository(User).findOne(user.id)
+    }
+
     return await assignedRolesByUser(user, context.state.domain)
   }
 }
