4363125ce3cfaa813916fd4e4a586755d6e1b4fb
leemyunggyu <jaylee@hatiolab.com>
2019-12-23 15:59:05 +0900

    v1.0.0-alpha.53

--- server/graphql/types/partner/index.ts
+++ server/graphql/types/partner/index.ts
@@ -16,7 +16,7 @@ export const Mutation = `
     role: ObjectRef!
     bizplaces: [ObjectRef]
     selfAssignment: Boolean
-  ): [BizplaceRoleAssignment]
+  ): BizplaceRoleAssignment
 `
 
 export const Query = `
